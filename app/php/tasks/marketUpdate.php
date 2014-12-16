fa0d1d14e48641355fb0b104950dd269078087bb
Titus Fogg <titusfogg@gmail.com>
2014-12-16 19:29:59 +0000

    Improved marketUpdate script and display of results

--- app/php/tasks/marketUpdate.php
+++ app/php/tasks/marketUpdate.php
@@ -1,6 +1,6 @@
 <?php
 
-ini_set('max_execution_time', 300);
+ini_set('max_execution_time', 0);
 
 // http://steamcommunity.com/market/priceoverview/?country=US&currency=3&appid=730&market_hash_name=StatTrak%E2%84%A2%20P250%20%7C%20Steel%20Disruption%20%28Factory%20New%29
 //For each item in the database: get market price.
@@ -12,6 +12,8 @@ ini_set('max_execution_time', 300);
   "median_price": "1,60&#8364; "
   }
  */
+
+echo "Script started: ".date("H:i:s")."<br/>";
 
 include_once("../config.php");
 $host = $config["db_host"];
@@ -37,25 +39,33 @@ $stmt->bindParam(':lowest_price', $lowest_price);
 $stmt->bindParam(':median_price', $median_price);
 $stmt->bindParam(':volume', $volume);
 // Iterate over each item from the DB.
+$successCount = 0;
+$attemptCount = 0;
 foreach ($results as $result) {
+    $attemptCount++;
     $defindex = $result['defindex'];
     $item_name = $result['name'];
-
+    echo "<strong>$defindex</strong> - $item_name <br/>";
     $url = "http://steamcommunity.com/market/priceoverview/?currency=1&appid=570&market_hash_name=" . rawurlencode($item_name);
     echo "getting contents of: " . $url . "<br />";
     $json_file = @file_get_contents($url);
     $json_decoded = json_decode($json_file);
     if (isset($json_decoded->success)) {
         if ($json_decoded->success) {
+            $successCount++;
             $lowest_price = isset($json_decoded->lowest_price) ? formatPrice($json_decoded->lowest_price) : null;
             $median_price = isset($json_decoded->median_price) ? formatPrice($json_decoded->median_price) : null;
             $volume = isset($json_decoded->volume) ? formatPrice($json_decoded->volume) : null;
-            echo $item_name . " is currently worth " . $json_decoded->median_price . "-------  <br />";
+            echo "Currently worth " . $median_price . "<br />";
             $stmt->execute();
+        } else {
+            echo "<span style='font-color:red;'>Market Get Fail</span>";
         }
+    } else {
+        echo "<p style='color:red;'>Success Not Set. File contents appear as: </p>";
+        echo $json_file;
     }
+    echo "--------------------------------------------------------- <br/>";
 }
-//$j = file_get_contents("http://steamcommunity.com/market/priceoverview/?currency=3&appid=570&market_hash_name=Blue%20Stone%20Shoulder%20of%20the%20Voyager");
-//$jd = json_decode($j, true);
-//echo $jd['median_price'];
-//echo rawurlencode("rune of the foreseer");
+echo "<strong>".$successCount."</strong> items scraped (out of $attemptCount) <br/>";
+echo "Script finished: ".date("H:i:s");
